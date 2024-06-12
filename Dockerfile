79b72b5636871baf8694340987ca82591b18e5d2
halfenif <halfenif@gmail.com>
2024-06-12 17:45:33 +0900

    1st 성공

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,9 @@
 FROM container-registry.oracle.com/database/express:latest
 
-COPY init.sql /docker-entrypoint-initdb.d/
+COPY init.sql /docker-entrypoint-initdb.d/startup/
+#RUN  chown oracle /docker-entrypoint-initdb.d/setup/init.sql
+
+ENV  ORACLE_PWD manager
 
 # ORACLE_CHARACTERSET: The character set to use when creating the database (default: AL32UTF8)
 # UTF8(old), AL32UTF8(new, not support under 8i)
--- init.sql
+++ init.sql
@@ -1,16 +1,23 @@
--- https://www.oracle.com/za/database/free/get-started/
--- To connect to the first Pluggable Database (PDB) use:
--- sqlplus sys@localhost:1521/FREEPDB1 as sysdba
--- To connect to the Container Database (CDB) use:
--- sqlplus sys@localhost:1521/FREE as sysdba
-alter session set container=FREEPDB1;
+-- https://www.oracle.com/kr/database/technologies/appdev/xe/quickstart.html
+-- https://container-registry.oracle.com/ords/f?p=113:4:100037901111748:::4:P4_REPOSITORY,AI_REPOSITORY,AI_REPOSITORY_NAME,P4_REPOSITORY_NAME,P4_EULA_ID,P4_BUSINESS_AREA_ID:803,803,Oracle%20Database%20Express%20Edition,Oracle%20Database%20Express%20Edition,1,0&cs=36cYAmVEznYF-CzTC7QYezU7V9e5N-vRUDXiAhOhSs19D9UOGruuW1zswvm2j1XM5Rs45l6nJDXqQgf82Cu5vPA
 
-create user myuser no authentication;
-grant connect, resource to myuser;
-grant unlimited tablespace to myuser; -- fine-tune this later
+CREATE TABLESPACE userdb
+DATAFILE 'userdb.dbf'
+SIZE 1000M
+AUTOEXTEND ON NEXT 10M;
 
-CREATE TABLE myuser.INVOICE (
-   EXTERNAL_ID NUMBER(10) NOT NULL,
-   DUEDATE DATE,
-   PRIMARY KEY (EXTERNAL_ID)
-);
+alter session set "_ORACLE_SCRIPT"=true;
+
+CREATE USER userdb
+IDENTIFIED BY userdb
+DEFAULT TABLESPACE userdb
+TEMPORARY TABLESPACE TEMP
+;
+
+grant CREATE SESSION, 
+      ALTER SESSION, 
+      CREATE SEQUENCE, 
+      CREATE TABLE, 
+      CREATE VIEW, 
+      UNLIMITED TABLESPACE 
+  to userdb;
